52a7a8179d7d22842cf722ce354d0150ded8be47
Matt Hampel <matth@localdata.com>
2014-01-22 15:46:25 -0800

    Remove remotegeojson from this PR

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var Map = require(__dirname + '/lib/Map'),
     GeoJson = require(__dirname + '/datasources/GeoJson'),
-    RemoteGeoJson = require(__dirname + '/datasources/RemoteGeoJson'),
     PostGIS = require(__dirname + '/datasources/PostGIS'),
     Shp = require(__dirname + '/datasources/Shp'),
     projector = require(__dirname + '/lib/projector'),
@@ -16,7 +15,6 @@ module.exports = {
    */
   datasources: {
     GeoJson: GeoJson,
-    RemoteGeoJson: RemoteGeoJson,
     PostGIS: PostGIS,
     Shp: Shp
   },
