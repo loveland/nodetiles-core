948f37babbfad7ebac33e7d2450c60893bb6bbee
Matt Hampel <matth@localdata.com>
2014-01-22 15:36:56 -0800

    Remove routes.js; unused?

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var Map = require(__dirname + '/lib/Map'),
     PostGIS = require(__dirname + '/datasources/PostGIS'),
     Shp = require(__dirname + '/datasources/Shp'),
     projector = require(__dirname + '/lib/projector'),
-    routes = require(__dirname + '/lib/routes'),
     UTFGrid = require(__dirname + '/lib/utfgrid');
 
 module.exports = {
@@ -24,10 +23,5 @@ module.exports = {
   /**
    * Transform between projections
    */
-  projector: projector,
-
-  /**
-   * Routing Middleware
-   */
-   route: routes
-}
+  projector: projector
+};
