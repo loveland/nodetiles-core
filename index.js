fc86f1bf7320390f6c8e207e47d25acd72fe2001
Matt Hampel <matth@localdata.com>
2014-01-22 16:19:00 -0800

    Re-add routes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Map = require(__dirname + '/lib/Map'),
     GeoJson = require(__dirname + '/datasources/GeoJson'),
-    PostGIS = require(__dirname + '/datasources/PostGIS'),
     Shp = require(__dirname + '/datasources/Shp'),
     projector = require(__dirname + '/lib/projector'),
+    routes = require(__dirname + '/lib/routes'),
     UTFGrid = require(__dirname + '/lib/utfgrid');
 
 module.exports = {
@@ -15,11 +15,15 @@ module.exports = {
    */
   datasources: {
     GeoJson: GeoJson,
-    PostGIS: PostGIS,
     Shp: Shp
   },
   /**
    * Transform between projections
    */
-  projector: projector
-};
+  projector: projector,
+
+  /**
+   * Routing Middleware
+   */
+   route: routes
+}
